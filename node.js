81a2741d8ca3a94c754deda41db62038ed5babc0
Ali Comert <74716393+alicomert@users.noreply.github.com>
2025-02-10 04:19:31 +0300

    Update node.js

--- node.js
+++ node.js
@@ -1,39 +1,38 @@
 const http = require('http');
 const axios = require('axios');
-const moment = require('moment-timezone'); // İstanbul zaman dilimi için
+const moment = require('moment-timezone');
+const https = require('https');
 
+// Telegram mesaj gönderme fonksiyonu
 async function sendTelegramMessage(chatId, text) {
   const telegramAPI = `https://api.telegram.org/bot6624881831:AAGeAR9ZULEZzcWOjYI3zBH03xUQxeVKBHY/sendMessage`;
   try {
-    await axios.post(telegramAPI, {
-      chat_id: chatId,
-      text: text,
-    });
+    await axios.post(telegramAPI, { chat_id: chatId, text: text });
   } catch (error) {
     console.error('Telegram mesajı gönderilirken hata oluştu:', error);
   }
 }
 
+// Zaman aralığını kontrol eden fonksiyon
 function checkTimeWindow(schedule) {
-  if (!schedule.start || !schedule.end) {
-    return true;
-  }
-
-  const now = moment().tz('Europe/Istanbul'); // Şu anki zaman İstanbul'a göre
-  const startTime = schedule.start.split(':').map(Number);
-  const endTime = schedule.end.split(':').map(Number);
-  
-  const startDate = moment().tz('Europe/Istanbul').set({ hour: startTime[0], minute: startTime[1], second: 0 });
-  const endDate = moment().tz('Europe/Istanbul').set({ hour: endTime[0], minute: endTime[1], second: 0 });
-
-  return now.isBetween(startDate, endDate);
+  if (!schedule.start || !schedule.end) return true;
+  const now = moment().tz('Europe/Istanbul');
+  const startTime = moment().tz('Europe/Istanbul').set({ hour: schedule.start.split(':')[0], minute: schedule.start.split(':')[1], second: 0 });
+  const endTime = moment().tz('Europe/Istanbul').set({ hour: schedule.end.split(':')[0], minute: schedule.end.split(':')[1], second: 0 });
+  return now.isBetween(startTime, endTime);
 }
 
+// URL'den veri çekme fonksiyonu
 async function fetchUrl(url, schedule) {
   if (checkTimeWindow(schedule)) {
     try {
       console.log(`${url} üzerinden veri çekiliyor...`);
-      const response = await axios.get(url);
+      const agent = new https.Agent({ rejectUnauthorized: false });
+      const response = await axios.get(url, {
+        headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' },
+        timeout: 10000,
+        httpsAgent: agent
+      });
       console.log(response.data);
     } catch (error) {
       console.error('Hata:', error);
@@ -43,20 +42,21 @@ async function fetchUrl(url, schedule) {
   }
 }
 
+// Zamanlama ile veri çekme fonksiyonu
 async function fetchDataWithTiming(urls, schedules) {
   const fetchPromises = urls.map((url, index) => {
     const schedule = schedules[index];
     return (async () => {
       while (true) {
         await fetchUrl(url, schedule);
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Her URL için bekleme süresi
+        await new Promise(resolve => setTimeout(resolve, 1000));
       }
     })();
   });
-
   await Promise.all(fetchPromises);
 }
 
+// URL listesi
 const urls = [
   'https://api.borsify.com/image/img.php',
   'https://finansliyorum.com/sitemap.php',
@@ -65,25 +65,24 @@ const urls = [
   'https://borsify.com/oto/savemumveri.php',
 ];
 
+// Zamanlama ayarları
 const schedules = [
   { start: '10:20', end: '11:00', delay: 0 },
   { delay: 86400000 },
   { start: '09:57', end: '18:55', delay: 7000 },
   { start: '09:57', end: '18:55', delay: 7000 },
   { start: '04:10', end: '18:55', delay: 5 },
-
-
-
 ];
 
-// Yarım saatte bir Telegram'a "Node.js aktif" mesajı gönder
+// Telegram'a belirli aralıklarla mesaj gönderme
 setInterval(() => {
   sendTelegramMessage(930115244, 'Node.js aktif');
-}, 1800000); // 1800000 ms = 30 dakika
+}, 1800000);
 
+// Veri çekme işlemini başlat
 fetchDataWithTiming(urls, schedules);
 
-// HTTP sunucusu oluştur ve belirli bir portta dinle
+// HTTP sunucusu oluştur
 const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
